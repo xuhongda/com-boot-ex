f0da5b878fa921f24024bd528f0fb1caebd5b2fe
xuhongda <xuhongda7@outlook.com>
2018-09-08 22:03:35 +0800

    spring reactive & pom

--- com-boot-web/src/main/java/com/xu/bootweb/WebApplication.java
+++ com-boot-web/src/main/java/com/xu/bootweb/WebApplication.java
@@ -1,8 +1,10 @@
 package com.xu.bootweb;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author xuhongda
@@ -11,7 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.xu.bootweb.mybatis")
 public class WebApplication {
 
+    @Bean
+    public ExitCodeGenerator exitCodeGenerator() {
+        return () -> 42;
+    }
+
     public static void main(String[] args) {
+        // System.exit(SpringApplication.exit(SpringApplication.run(WebApplication.class,args)));
         SpringApplication.run(WebApplication.class, args);
     }
 }
--- com-boot-web/src/main/java/com/xu/bootweb/controller/HelloController.java
+++ com-boot-web/src/main/java/com/xu/bootweb/controller/HelloController.java
@@ -4,10 +4,8 @@ import com.xu.bootweb.MyRepository;
 import com.xu.bootweb.mybatis.UserDao;
 import entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -39,6 +37,11 @@ public class HelloController {
     @GetMapping("test2")
     public List<UserEntity> test2() {
         return userDao.getAll();
+    }
+
+    @GetMapping("test3")
+    public void test3() {
+        throw new RuntimeException();
     }
 
 }
