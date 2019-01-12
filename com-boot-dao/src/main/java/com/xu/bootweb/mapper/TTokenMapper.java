0be68e2a770f4d651c0804ac646859af4a84dc63
xuhongda <xuhongda7@outlook.com>
2019-01-12 22:18:06 +0800

    数据库事务

---
+++ com-boot-dao/src/main/java/com/xu/bootweb/mapper/TTokenMapper.java
@@ -0,0 +1,30 @@
+package com.xu.bootweb.mapper;
+
+import com.xu.bootweb.pojo.TToken;
+import com.xu.bootweb.pojo.TTokenExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TTokenMapper {
+    long countByExample(TTokenExample example);
+
+    int deleteByExample(TTokenExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TToken record);
+
+    int insertSelective(TToken record);
+
+    List<TToken> selectByExample(TTokenExample example);
+
+    TToken selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TToken record, @Param("example") TTokenExample example);
+
+    int updateByExample(@Param("record") TToken record, @Param("example") TTokenExample example);
+
+    int updateByPrimaryKeySelective(TToken record);
+
+    int updateByPrimaryKey(TToken record);
+}
---
+++ com-boot-web/src/main/java/com/xu/bootweb/controller/TransactionController.java
@@ -0,0 +1,57 @@
+package com.xu.bootweb.controller;
+
+import com.xu.bootweb.mapper.TTokenMapper;
+import com.xu.bootweb.mapper.TUserMapper;
+import com.xu.bootweb.pojo.TToken;
+import com.xu.bootweb.pojo.TUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ *
+ * @author xuhongda on 2019/1/12
+ * com.xu.bootweb.controller
+ * com-boot-ex
+ */
+@RestController
+@RequestMapping("t")
+public class TransactionController {
+    private final TUserMapper tUserMapper;
+
+    private final TTokenMapper tTokenMapper;
+
+    @Autowired
+    public TransactionController(TUserMapper tUserMapper, TTokenMapper tTokenMapper) {
+        this.tUserMapper = tUserMapper;
+        this.tTokenMapper = tTokenMapper;
+    }
+
+    /**
+     * 数据库事务
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @GetMapping("t1")
+    public void t1(){
+        TUser tUser = new TUser();
+        tUser.setUsername("xcv");
+        tUser.setLoginacct(new Date().toString());
+        tUser.setEmail("xxx");
+        tUser.setUserpswd("1234");
+        tUserMapper.insertSelective(tUser);
+        m1();
+    }
+
+    private void m1() {
+        TToken tToken = new TToken();
+        tToken.setToken("xxxxxxxxx");
+        tToken.setUid(11111);
+        tToken.setCreatetime(new Date());
+        tTokenMapper.insertSelective(tToken);
+        throw new RuntimeException();
+    }
+}
