0529ddff0e29e12c9ac8d4a59fcc27bb52f3648f
xuhongda <xuhongda7@outlook.com>
2019-02-24 17:52:49 +0800

    格式化

--- com-boot-web/src/main/java/com/xu/bootweb/WebApplication.java
+++ com-boot-web/src/main/java/com/xu/bootweb/WebApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 /**
