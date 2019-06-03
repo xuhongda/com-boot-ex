92651d1eb07140dc6258457feee3795f6f102cdc
xuhongda <xuhongda7@outlook.com>
2019-06-03 11:56:40 +0800

    PropertiesConfiguration 读取 Properties 文件

--- com-boot-web/src/test/java/com/xu/bootweb/resourceTest/ResourceTest.java
+++ com-boot-web/src/test/java/com/xu/bootweb/resourceTest/ResourceTest.java
@@ -2,6 +2,9 @@ package com.xu.bootweb.resourceTest;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,16 +14,22 @@ import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author xuhongda on 2018/10/13
  * com.xu.bootweb.resourceTest
  * com-boot-ex
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ResourceTest {
+
+
+    private static final double C = Math.PI / 180;
 
     /**
      * spring 路径
@@ -38,5 +47,20 @@ public class ResourceTest {
         List<String> strings = Files.readLines(file1, Charsets.UTF_8);
         strings.forEach(System.out::println);
     }
+
+    /**
+     * {@link PropertiesConfiguration} 读取Properties配置文件
+     * @throws  ConfigurationException exception
+     */
+    @Test
+    public void test2() throws ConfigurationException {
+        String fileName = "test.properties";
+        PropertiesConfiguration propertiesConfiguration = new PropertiesConfiguration(fileName);
+        String name = (String)propertiesConfiguration.getProperty("name");
+        log.info("name = {}",name);
+    }
+
+
+
 
 }
