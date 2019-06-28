8787b41a67f79a876e8aae45c8a71445b7ea842a
xuhongda <xuhongda7@outlook.com>
2019-06-28 08:57:36 +0800

    web config

--- com-boot-core/src/main/java/com/xu/bootweb/config/WebMvcConfig.java
+++ com-boot-core/src/main/java/com/xu/bootweb/config/WebMvcConfig.java
@@ -1,9 +1,8 @@
 package com.xu.bootweb.config;
 
+import com.xu.bootweb.interceptor.MyInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * <p>
@@ -17,12 +16,33 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/hello/**")
+                .excludePathPatterns("/admin/**");
+
+    }
+
     /**
      * 拦截 url 中带有 /static 的请求 ，映射到 static 目录
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/").setCachePeriod(31556926);
+        //favicon.ico 图标
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/").setCachePeriod(31556926);
+    }
+
+    /**
+     * <mvc:view-controller path="/user/register" view-name="demo-signUp"/>
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/user/register").setViewName("demo-signUp");
     }
 
 }
