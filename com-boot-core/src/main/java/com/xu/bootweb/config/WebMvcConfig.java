425e482cfd9094bee70866c4e4fbe8fb80a271ff
xuhongda <xuhongda7@outlook.com>
2019-08-30 18:04:30 +0800

    字符编码问题解决

--- com-boot-core/src/main/java/com/xu/bootweb/config/WebMvcConfig.java
+++ com-boot-core/src/main/java/com/xu/bootweb/config/WebMvcConfig.java
@@ -1,9 +1,17 @@
 package com.xu.bootweb.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xu.bootweb.interceptor.MyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * <p>
@@ -49,6 +57,42 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user/register").setViewName("demo-signUp");
         registry.addViewController("/socket").setViewName("socket");
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        converter.setWriteAcceptCharset(false);
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter messageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(getObjectMapper());
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        //super.configureMessageConverters(converters);
+        //解决中文乱码
+        converters.add(responseBodyConverter());
+        //解决 添加解决中文乱码后 上述配置之后，返回json数据直接报错 500：no convertter for return value of type
+        converters.add(messageConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
     }
 
 }
