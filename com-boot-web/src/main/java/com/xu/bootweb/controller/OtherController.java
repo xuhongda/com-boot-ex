0df5af3c58ad9cb7851a47c5cc6515b2df092085
xuhongda <xuhongda7@outlook.com>
2019-09-10 11:22:37 +0800

    clean code

--- com-boot-web/src/main/java/com/xu/bootweb/controller/OtherController.java
+++ com-boot-web/src/main/java/com/xu/bootweb/controller/OtherController.java
@@ -9,8 +9,6 @@ import entity.Result;
 import entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
